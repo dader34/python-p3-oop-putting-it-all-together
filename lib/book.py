0feb16f4d9746afaf25bb2a168ed6804c03725b0
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-24 19:17:08 -0700

    Passed all tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title,page_count):
+        self._title = title
+        if(type(page_count) != int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
     
+    @title.setter
+    def title(self,title):
+        if(type(title) != str):
+            print("title must be a string")
+        else:
+            self._title = title
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self,page_count):
+        if(type(page_count) != int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
         
+book = Book("The Great Gatsby", 300)
+print(book)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        if(type(size) != int):
+            print("size must be an integer")
+        else:
+            self._size = size
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self,size):
+        if(type(size) != int):
+            print("size must be an integer")
+        else:
+            self._size = size
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
